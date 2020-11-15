2a7d5a33cf85072db13ce0cc134d3782b107ee02
Fiona Weaver <fiona.l.weaver@gmail.com>
2020-11-15 12:41:45 -0700

    commit tree 9b0c0d8db5b9634ff31ec6ac0e8e0113badea1c7

--- lib/anagram.rb
+++ lib/anagram.rb
@@ -1,1 +1,15 @@
-# Your code goes here!
+class Anagram 
+    attr_accessor :anagram
+    
+    def initialize(anagram)
+        @anagram = anagram 
+    end 
+
+    def match(array)
+     array.select do |word|
+        (@anagram.split("").sort) == (word.split("").sort)
+     end 
+   end 
+end 
+
+
